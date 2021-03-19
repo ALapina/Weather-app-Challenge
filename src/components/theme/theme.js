c844c9289ee30fdd3025835a0583ae44a4d4f939
ALapina <larisa_42@mail.ru>
2021-03-19 11:57:11 +0200

    Setup initial styles styled components css color variables (themes) https://styled-components.com/docs/advanced#theming font family and weight (Raleway) global styles https://scalablecss.com/styled-components-global-styles/ styled-normalize https://github.com/sergeysova/styled-normalize

---
+++ src/components/theme/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primary: "#1E213A",
+    background: "#100E1D",
+    light: "#E7E7EB",
+    grey: "#A09FB1",
+    lightGrey: "##6E707A",
+    darkGrey: "#88869D",
+    searchGrey: "#616475",
+    buttonBackground: "#3C47E9",
+  },
+};
